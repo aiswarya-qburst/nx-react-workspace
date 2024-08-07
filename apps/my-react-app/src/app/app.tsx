29746cc376f34f76bea14dbd200a7acafa2b29f0
aiswarya-qburst <aiswaryav@qburst.com>
2024-08-07 14:52:55 +0530

    Added shared library

--- apps/my-react-app/src/app/app.tsx
+++ apps/my-react-app/src/app/app.tsx
@@ -3,9 +3,12 @@ import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
 
+import { UiShared } from '@nx-react-workspace/ui-shared';
+
 export function App() {
   return (
     <div>
+      <UiShared />
       <NxWelcome title="my-react-app" />
     </div>
   );
